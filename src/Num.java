9dbb607ff4868b9ac9561609210f411aca8082be
thefluffyoshi <katielynntrinh@gmail.com>
2019-10-18 13:18:55 -0700

    Finished

--- src/Num.java
+++ src/Num.java
@@ -1,5 +1,24 @@
 public class Num
 {
     private int value;
-    
-}
+    //Sets up new Num object, storing an initial value
+    public Num (int update)
+    {
+        value = update;
+    }//end Num
+    //Returns stored integer value
+    public int getValue ()
+    {
+        return value;
+    }//end int getValue
+    //Sets stored value to newly specified value
+    public void setValue (int update)
+    {
+        value = update;
+    }//end setValue
+    //Returns stored integer value as a string
+    public String toString ()
+    {
+        return value + "";
+    }
+}//end class Num
--- src/References.java
+++ src/References.java
@@ -13,5 +13,20 @@ public class References
         System.out.println("n1: " + n1);
         System.out.println("n1: " + n2);
         System.out.println("i1: " + i1);
+        System.out.println();
+        n1 = n2;
+        i1 = i2;
+        System.out.println("n1: " + n1);
+        System.out.println("n2: " + n2);
+        System.out.println("i1: " + i1);
+        System.out.println("i2: " + i2);
+        System.out.println();
+        n1.setValue(n1.getValue()+1);
+        i1++;
+        System.out.println("n1: " + n1);
+        System.out.println("n2: " + n2);
+        System.out.println("i1: " + i1);
+        System.out.println("i2: " + i2);
+        System.out.println();
     }//end main
 }//end class References
